a2b19e7489e91dcef985e23ea20a5cd28981711e
Ivan Policarpio <ivanpolicarpio015@gmail.com>
2025-11-17 08:24:39 +0800

    fix mobile size view and dark mode

--- components/SideBar/SideBar.jsx
+++ components/SideBar/SideBar.jsx
@@ -8,10 +8,9 @@ import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react";
 
 export default function SideBar({setShowLoginModal}){
-    const [isLogin, setIsLogin] = useState(false);
     const [cartCount, setCartCount] = useState(false);
     const [isOpen, setIsOpen] = useState(false);
-    const { theme, setTheme } = useTheme();
+    const {theme, setTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
     const { data: session } = useSession();
     
@@ -25,7 +24,7 @@ export default function SideBar({setShowLoginModal}){
 
     return(
         <>  
-            {isOpen ? (<X  className="fixed top-3 right-9 lg:hidden" size={30} onClick={() => setIsOpen(false)}/>) : (<Menu className="fixed top-3 left-3 lg:hidden" onClick={() => setIsOpen(true)} size={30}/>) }
+            {isOpen ? (<X  className="fixed top-3 right-27 z-22 cursor-pointer lg:hidden" size={30} onClick={() => setIsOpen(false)}/>) : (<Menu className="absolute top-3 left-3 lg:hidden cursor-pointer" onClick={() => setIsOpen(true)} size={30}/>) }
             <div className={`fixed top-0 left-0 h-screen bg-gray-100  px-5 shadow-lg z-20 transform transition-transform duration-500 ease-in-out w-[80%] lg:flex lg:h-25 lg:w-full lg:translate-x-0 ${isOpen ? "translate-x-0" : "-translate-x-full"} ${theme === 'dark' ? 'bg-gray-500' : ''}`}>
                 <img src='/Logo/landing.png' alt="Poli Collective Logo" title="Poli Collective Logo" className="w-[100px] cursor-pointer"/>
                 <div className=" text-lg flex flex-col lg:flex-row lg:items-center lg:w-full lg:justify-end">
@@ -33,18 +32,27 @@ export default function SideBar({setShowLoginModal}){
                     <Link href="/Shop_Now" className="px-5 py-3 hover:text-blue-500 transition duration-500">Shop Now</Link>
                     <Link href="/" className="px-5 py-3 hover:text-blue-500 transition duration-500">About</Link>
                     <Link href="/" className="px-5 py-3 hover:text-blue-500 transition duration-500">Contact</Link>
-                    <div className="flex flex-col py-3">
-                        {cartCount ? (<div className="bg-red-500 text-white w-5 h-5 flex items-center justify-center rounded-full text-sm ml-8">1</div>) : (<div className="hidden">1</div>)}
+                    <div className="lg:flex flex-col lg:items-center py-3 hidden">
+                        {cartCount ? (<div className="absolute bg-red-500 text-white w-5 h-5 flex items-center justify-center rounded-full text-sm ml-8 -mt-4 lg:ml-5">1</div>) : (<div className="hidden">1</div>)}
                         <Link href="/" className="px-5 hover:text-blue-500 transition duration-500 "><ShoppingCart size={25}/></Link>
                     </div>
-                    <div className="px-5">
+                    <div className="px-5 hidden lg:block">
                         {session ? (<img src={session.user.image} alt="Poli Collective Logo" title={session.user.name}className="w-10 cursor-pointer rounded-full"/>) : ( <button className="px-5 cursor-pointer bg-black h-10 w-22 text-white rounded-md hover:bg-gray-700 transition duration-500 text-sm" onClick={() => setShowLoginModal(true)}>Login</button>)}
                     </div>
                     <div className="px-5 py-3 lg:px-0">
                         <DarkMode theme={theme} setTheme={setTheme}/>
                     </div>
-                    {session ? (<div className="px-5"><LogOut className="cursor-pointer hover:text-blue-500 transition duration-500" onClick={() => signOut("google")} /></div>) : (<div className="hidden">asd</div>)}
+                    {session ? (<div className="px-5"><LogOut className="cursor-pointer hover:text-blue-500 transition duration-500 hidden lg:block" onClick={() => signOut("google")} /></div>) : (<div className="hidden">asd</div>)}
                 </div>
+            </div>
+            <div className="flex items-center justify-end py-3 px-3 lg:hidden">
+                <div className="flex flex-col items-center justify-center">
+                    {cartCount ? (<div className="absolute bg-red-500 text-white w-5 h-5 flex items-center justify-center rounded-full text-sm top-1 ml-5">1</div>) : (<div className="hidden">1</div>)}
+                    <Link href="/" className="px-5 hover:text-blue-500 transition duration-500 "><ShoppingCart size={25}/></Link>
+                </div>
+                {session ? (<img src={session.user.image} alt="Poli Collective Logo" title={session.user.name}className="w-10 cursor-pointer rounded-full"/>) : ( <button className="px-5 cursor-pointer bg-black h-10 w-22 text-white rounded-md hover:bg-gray-700 transition duration-500 text-sm" onClick={() => setShowLoginModal(true)}>Login</button>)}
+                {session ? (<div className="pl-3"><LogOut className="cursor-pointer hover:text-blue-500 transition duration-500 " onClick={() => signOut("google")} /></div>) : (<div className="hidden">asd</div>)}
+      
             </div>
         </>
     );

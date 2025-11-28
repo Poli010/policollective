3d9f3e8f12a7b1e655769427b3f38a218eb78836
Ivan Policarpio <ivanpolicarpio015@gmail.com>
2025-11-28 08:56:26 +0800

    responsive ui for mobile devices

--- app/Admin_Page/Products/page.jsx
+++ app/Admin_Page/Products/page.jsx
@@ -17,14 +17,14 @@ export default function Products(){
     return(
         <>
             <div className="lg:flex max-w-400 mx-auto">
-                <Admin_SideBar idebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/>
+                <Admin_SideBar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/>
                 <div className={`w-full pt-5 px-4 md:px-7 bg-gray-50 dark:bg-gray-950 ${isSidebarOpen ? 'h-screen overflow-hidden' : ''}`}>
                     <Admin_Header/>
-                    <div className="bg-white h-[83vh] shadow-2xl mt-3 dark:bg-gray-900 rounded-md">
-                        <div className="grid grid-cols-4 gap-5 p-5">
-                            <button className="flex items-center bg-black hover:bg-gray-700 text-white px-3 py-1.5 w-[180px] rounded-md text-sm cursor-pointer"><Plus/> Add Products</button>
+                    <div className="bg-white h-auto md:h-[83vh] mt-3 dark:bg-gray-900 rounded-md">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 p-5">
+                            <button className="flex items-center h-10 bg-black hover:bg-gray-700 text-white px-3 py-1.5 w-full rounded-md text-sm cursor-pointer"><Plus/> Add Products</button>
                             <Select>
-                                <SelectTrigger className="w-[180px] border-black dark:border-white">
+                                <SelectTrigger className="w-full border-black dark:border-white">
                                     <SelectValue placeholder="Status" />
                                 </SelectTrigger>
                                 <SelectContent>
@@ -34,7 +34,7 @@ export default function Products(){
                                 </SelectContent>
                             </Select>
                             <Select>
-                                <SelectTrigger className="w-[180px] border-black dark:border-white">
+                                <SelectTrigger className="w-full border-black dark:border-white">
                                     <SelectValue placeholder="Category" />
                                 </SelectTrigger>
                                 <SelectContent>
@@ -43,10 +43,10 @@ export default function Products(){
                                     <SelectItem value="system">Shoes</SelectItem>
                                 </SelectContent>
                             </Select>
-                            <input type="search" className="border border-black rounded-md px-2 outline-blue-500 dark:border-white w-[180px]" placeholder={`Search here`}/>
+                            <input type="search" className="border border-black rounded-md px-2 outline-blue-500 dark:border-white w-full h-9" placeholder={`Search here`}/>
                         </div>
 
-                        <div className="h-[70vh] overflow-y-scroll overflow-x-hidden p-5">
+                        <div className="h-[70vh] md:overflow-y-scroll md:overflow-x-hidden p-5">
                             <Products_Data/>
                         </div>
                         
--- components/Products/Products_Item/products_item.jsx
+++ components/Products/Products_Item/products_item.jsx
@@ -6,21 +6,24 @@ import { PhilippinePeso, Pencil, Trash2 } from 'lucide-react';
 export default function Products_Item() {
     return (
         <>
-            <div className='grid gap-5 grid-cols-3 '>
+            <div className='grid gap-5 place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4'>
                 <div className="">
-                    <div className="w-60 shad bg-white dark:bg-gray-900 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 border border-gray-200 dark:border-gray-700">
-                        <div className="relative w-full h-[250px] rounded-t-xl overflow-hidden">
-                            <Image 
-                                src={tryImage}
-                                alt="temp"
-                                fill
-                                className="object-cover"
-                            />
-                            <p className="absolute top-2 right-2 bg-black/70 text-white text-xs px-2 py-1 rounded-md">20% OFF</p>
+                    <div className="w-86 md:w-60 bg-white dark:bg-gray-900 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 border border-gray-200 dark:border-gray-700">
+                        <div className='flex items-center justify-center'>
+                            <div className="relative w-60 h-[250px] rounded-t-xl overflow-hidden">
+                                <Image 
+                                    src={tryImage}
+                                    alt="temp"
+                                    fill
+                                    className="object-cover"
+                                />
+                                <p className="absolute top-2 right-2 bg-black/70 text-white text-xs px-2 py-1 rounded-md">20% OFF</p>
+                            </div>
                         </div>
-                        <div className="px-4 py-3">
+                        
+                        <div className="px-10 md:px-4 py-3">
                             <p className="font-semibold text-lg">Nissan Shirt</p>
-                            <p className="text-gray-600 dark:text-gray-300 text-sm">Category: T-Shirt</p>
+                            <p className="text-gray-600 dark:text-gray-300 text-sm mb-2">Category: T-Shirt</p>
                             <div className='flex flex-row just items-center'>
                                 <p className="text-gray-600 dark:text-gray-300 text-sm">Status: Active </p>
                                 <p className='bg-yellow-500 rounded-full w-3 h-3 ml-1'></p>
@@ -39,7 +42,7 @@ export default function Products_Item() {
                             </div>
                         </div>
                         {/* ACTION BUTTONS */}
-                        <div className="flex justify-between px-4 pb-4">
+                        <div className="flex justify-between px-10 md:px-4 pb-4">
                             <button className="flex items-center gap-1 bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
                                 <Pencil size={16} /> Edit
                             </button>
@@ -51,107 +54,27 @@ export default function Products_Item() {
                 </div>  
 
                 <div className="">
-                    <div className="w-60 shad bg-white dark:bg-gray-900 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 border border-gray-200 dark:border-gray-700">
-                        <div className="relative w-full h-[250px] rounded-t-xl overflow-hidden">
-                            <Image 
-                                src={tryImage}
-                                alt="temp"
-                                fill
-                                className="object-cover"
-                            />
-                            <p className="absolute top-2 right-2 bg-black/70 text-white text-xs px-2 py-1 rounded-md">20% OFF</p>
-                        </div>
-                        <div className="px-4 py-3">
-                            <p className="font-semibold text-lg">Nissan Shirt</p>
-                            <p className="text-gray-600 dark:text-gray-300 text-sm mb-2">Category: T-Shirt</p>
-                            <div className='flex flex-row just items-center'>
-                                <p className="text-gray-600 dark:text-gray-300 text-sm">Status: Inactive </p>
-                                <p className='bg-gray-500 rounded-full w-3 h-3 ml-1'></p>
-                            </div>
-                            <div className="flex justify-between mt-2">
-                                <div>
-                                    <p className="text-sm text-gray-500">Price:</p>
-                                    <p className="flex items-center font-semibold"><PhilippinePeso size={18} className="mr-1" /> 250</p>
-                                </div>
-
-                                <div>
-                                    <p className="text-sm text-gray-500">Stock:</p>
-                                    <p className="font-semibold text-center">5</p>
-                                </div>
+                    <div className="w-86 md:w-60 bg-white dark:bg-gray-900 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 border border-gray-200 dark:border-gray-700">
+                        <div className='flex items-center justify-center'>
+                            <div className="relative w-60 h-[250px] rounded-t-xl overflow-hidden">
+                                <Image 
+                                    src={tryImage}
+                                    alt="temp"
+                                    fill
+                                    className="object-cover"
+                                />
+                                <p className="absolute top-2 right-2 bg-black/70 text-white text-xs px-2 py-1 rounded-md">20% OFF</p>
                             </div>
                         </div>
-                        {/* ACTION BUTTONS */}
-                        <div className="flex justify-between px-4 pb-4">
-                            <button className="flex items-center gap-1 bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
-                                <Pencil size={16} /> Edit
-                            </button>
-                            <button className="flex items-center gap-1 bg-red-600 hover:bg-red-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
-                                <Trash2 size={16} /> Delete
-                            </button>
-                        </div>
-                    </div>
-                </div>  
-
-                <div className="">
-                    <div className="w-60 shad bg-white dark:bg-gray-900 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 border border-gray-200 dark:border-gray-700">
-                        <div className="relative w-full h-[250px] rounded-t-xl overflow-hidden">
-                            <Image 
-                                src={tryImage2}
-                                alt="temp"
-                                fill
-                                className="object-cover"
-                            />
-                            <p className="absolute top-2 right-2 bg-black/70 text-white text-xs px-2 py-1 rounded-md">20% OFF</p>
-                        </div>
-                        <div className="px-4 py-3">
-                            <p className="font-semibold text-lg">Nissan Shirt</p>
-                            <p className="text-gray-600 dark:text-gray-300 text-sm mb-2">Category: T-Shirt</p>
-                            <div className='flex flex-row just items-center'>
-                                <p className="text-gray-600 dark:text-gray-300 text-sm">Status: Out of Stock </p>
-                                <p className='bg-red-700 rounded-full w-3 h-3 ml-1'></p>
-                            </div>
-                            <div className="flex justify-between mt-2">
-                                <div>
-                                    <p className="text-sm text-gray-500">Price:</p>
-                                    <p className="flex items-center font-semibold"><PhilippinePeso size={18} className="mr-1" /> 250</p>
-                                </div>
-
-                                <div>
-                                    <p className="text-sm text-gray-500">Stock:</p>
-                                    <p className="font-semibold text-center">0</p>
-                                </div>
-                            </div>
-                        </div>
-                        {/* ACTION BUTTONS */}
-                        <div className="flex justify-between px-4 pb-4">
-                            <button className="flex items-center gap-1 bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
-                                <Pencil size={16} /> Edit
-                            </button>
-                            <button className="flex items-center gap-1 bg-red-600 hover:bg-red-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
-                                <Trash2 size={16} /> Delete
-                            </button>
-                        </div>
-                    </div>
-                </div>  
-
-                <div className="">
-                    <div className="w-60 shad bg-white dark:bg-gray-900 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 border border-gray-200 dark:border-gray-700">
-                        <div className="relative w-full h-[250px] rounded-t-xl overflow-hidden">
-                            <Image 
-                                src={tryImage2}
-                                alt="temp"
-                                fill
-                                className="object-cover"
-                            />
-                            <p className="absolute top-2 right-2 bg-black/70 text-white text-xs px-2 py-1 rounded-md">20% OFF</p>
-                        </div>
-                        <div className="px-4 py-3">
+                        
+                        <div className="px-10 md:px-4 py-3">
                             <p className="font-semibold text-lg">Nissan Shirt</p>
                             <p className="text-gray-600 dark:text-gray-300 text-sm mb-2">Category: T-Shirt</p>
                             <div className='flex flex-row just items-center'>
                                 <p className="text-gray-600 dark:text-gray-300 text-sm">Status: Active </p>
                                 <p className='bg-yellow-500 rounded-full w-3 h-3 ml-1'></p>
                             </div>
+                           
                             <div className="flex justify-between mt-2">
                                 <div>
                                     <p className="text-sm text-gray-500">Price:</p>
@@ -165,7 +88,99 @@ export default function Products_Item() {
                             </div>
                         </div>
                         {/* ACTION BUTTONS */}
-                        <div className="flex justify-between px-4 pb-4">
+                        <div className="flex justify-between px-10 md:px-4 pb-4">
+                            <button className="flex items-center gap-1 bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
+                                <Pencil size={16} /> Edit
+                            </button>
+                            <button className="flex items-center gap-1 bg-red-600 hover:bg-red-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
+                                <Trash2 size={16} /> Delete
+                            </button>
+                        </div>
+                    </div>
+                </div>  
+
+                <div className="">
+                    <div className="w-86 md:w-60 bg-white dark:bg-gray-900 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 border border-gray-200 dark:border-gray-700">
+                        <div className='flex items-center justify-center'>
+                            <div className="relative w-60 h-[250px] rounded-t-xl overflow-hidden">
+                                <Image 
+                                    src={tryImage}
+                                    alt="temp"
+                                    fill
+                                    className="object-cover"
+                                />
+                                <p className="absolute top-2 right-2 bg-black/70 text-white text-xs px-2 py-1 rounded-md">20% OFF</p>
+                            </div>
+                        </div>
+                        
+                        <div className="px-10 md:px-4 py-3">
+                            <p className="font-semibold text-lg">Nissan Shirt</p>
+                            <p className="text-gray-600 dark:text-gray-300 text-sm mb-2">Category: T-Shirt</p>
+                            <div className='flex flex-row just items-center'>
+                                <p className="text-gray-600 dark:text-gray-300 text-sm">Status: Active </p>
+                                <p className='bg-yellow-500 rounded-full w-3 h-3 ml-1'></p>
+                            </div>
+                           
+                            <div className="flex justify-between mt-2">
+                                <div>
+                                    <p className="text-sm text-gray-500">Price:</p>
+                                    <p className="flex items-center font-semibold"><PhilippinePeso size={18} className="mr-1" /> 250</p>
+                                </div>
+
+                                <div>
+                                    <p className="text-sm text-gray-500">Stock:</p>
+                                    <p className="font-semibold text-center">5</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* ACTION BUTTONS */}
+                        <div className="flex justify-between px-10 md:px-4 pb-4">
+                            <button className="flex items-center gap-1 bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
+                                <Pencil size={16} /> Edit
+                            </button>
+                            <button className="flex items-center gap-1 bg-red-600 hover:bg-red-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
+                                <Trash2 size={16} /> Delete
+                            </button>
+                        </div>
+                    </div>
+                </div>  
+
+                <div className="">
+                    <div className="w-86 md:w-60 bg-white dark:bg-gray-900 rounded-lg shadow-xl hover:shadow-2xl transition-shadow duration-300 border border-gray-200 dark:border-gray-700">
+                        <div className='flex items-center justify-center'>
+                            <div className="relative w-60 h-[250px] rounded-t-xl overflow-hidden">
+                                <Image 
+                                    src={tryImage2}
+                                    alt="temp"
+                                    fill
+                                    className="object-cover"
+                                />
+                                <p className="absolute top-2 right-2 bg-black/70 text-white text-xs px-2 py-1 rounded-md">20% OFF</p>
+                            </div>
+                        </div>
+                        
+                        <div className="px-10 md:px-4 py-3">
+                            <p className="font-semibold text-lg">Nissan Shirt</p>
+                            <p className="text-gray-600 dark:text-gray-300 text-sm mb-2">Category: T-Shirt</p>
+                            <div className='flex flex-row just items-center'>
+                                <p className="text-gray-600 dark:text-gray-300 text-sm">Status: Active </p>
+                                <p className='bg-yellow-500 rounded-full w-3 h-3 ml-1'></p>
+                            </div>
+                           
+                            <div className="flex justify-between mt-2">
+                                <div>
+                                    <p className="text-sm text-gray-500">Price:</p>
+                                    <p className="flex items-center font-semibold"><PhilippinePeso size={18} className="mr-1" /> 250</p>
+                                </div>
+
+                                <div>
+                                    <p className="text-sm text-gray-500">Stock:</p>
+                                    <p className="font-semibold text-center">5</p>
+                                </div>
+                            </div>
+                        </div>
+                        {/* ACTION BUTTONS */}
+                        <div className="flex justify-between px-10 md:px-4 pb-4">
                             <button className="flex items-center gap-1 bg-blue-600 hover:bg-blue-700 text-white px-3 py-1.5 rounded-md text-sm cursor-pointer">
                                 <Pencil size={16} /> Edit
                             </button>
